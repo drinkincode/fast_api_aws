55dfc4a9f5c5505453b15c8c8aeb7855642c3841
drinkincode] <jonnysmith696910@gmail.com>
2024-07-27 16:12:36 -0500

    dynamic port bugfix

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ app.include_router(oauth.router)
 app.include_router(api.router)
 
 # uvicorn.run(app)
-uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
+# uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
